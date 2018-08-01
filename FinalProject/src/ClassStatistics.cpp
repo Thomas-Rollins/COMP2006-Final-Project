49249b1a0764ca5c599b7028ce02b805aa0dbfd5
Thomas Rollins <Thomas.Rollins@hotmail.ca>
2018-08-01 16:47:49 -0400

    added containers for High level and elemental stats.

--- FinalProject/src/ClassStatistics.cpp
+++ FinalProject/src/ClassStatistics.cpp
@@ -12,7 +12,35 @@ const std::string* ClassStatistics::LOW_STAT_DESCRIPTIONS[] =
 	LOW_STAT_DESCRIPTIONS[vitality_id]		= (std::string*)	"Vitality Description Here",
 	LOW_STAT_DESCRIPTIONS[wisdom_id]		= (std::string*)	"Wisdom Description Here"
 };
+	//max value for any high_level stat: 9 999 999 999
+const double ClassStatistics::HIGH_STAT_MAX_VALUE = 9999999999;
 
+const std::string* ClassStatistics::HIGH_STAT_DESCRIPTIONS[] =
+{
+	HIGH_STAT_DESCRIPTIONS[health_id]		= (std::string*)	"Represents the life force. When this hits 0 you die.",
+	HIGH_STAT_DESCRIPTIONS[mana_id]			= (std::string*)	"Represents the ability to use magic. Without it you cannot use any magic based abilities.",
+	HIGH_STAT_DESCRIPTIONS[skill_points_id] = (std::string*)	"Represents the ability to activate skills. Most skills require skill points to activate. Skill points increase during battle when you take or deal damage.",
+	HIGH_STAT_DESCRIPTIONS[physical_atk_id] = (std::string*)	"Represents the ability to deal physical damage.",
+	HIGH_STAT_DESCRIPTIONS[def_id]			= (std::string*)	"Represents the ability to resist physical damage.",
+	HIGH_STAT_DESCRIPTIONS[magic_atk_id]	= (std::string*)	"Represents the ability to deal magic damage.",
+	HIGH_STAT_DESCRIPTIONS[mind_id]			= (std::string*)	"Represents the ability to resist magic damage.",
+	HIGH_STAT_DESCRIPTIONS[accuracy_id]		= (std::string*)	"Represents the likeliness of your attacks landing. This effects both Physical and Magical attacks.",
+	HIGH_STAT_DESCRIPTIONS[evasion_id]		= (std::string*)	"Represents the ability to dodge both physical and magical attacks.",
+	HIGH_STAT_DESCRIPTIONS[critical_id]		= (std::string*)	"Represents the ability exploit the target's weak points dealing massive damage."
+};
+
+const float ClassStatistics::ELEMENTAL_STAT_MAX_VALUE = 500.00f;
+const std::string* ClassStatistics::ELEMENTAL_STAT_DESCRIPTIONS[] =
+{
+	ELEMENTAL_STAT_DESCRIPTIONS[fire_id]		= (std::string*) "Represents your ability to utilize and resist the element of fire.",
+	ELEMENTAL_STAT_DESCRIPTIONS[wind_id]		= (std::string*) "Represents your ability to utilize and resist the element of wind.",
+	ELEMENTAL_STAT_DESCRIPTIONS[earth_id]		= (std::string*) "Represents your ability to utilize and resist the element of earth.",
+	ELEMENTAL_STAT_DESCRIPTIONS[water_id]		= (std::string*) "Represents your ability to utilize and resist the element of water.",
+	ELEMENTAL_STAT_DESCRIPTIONS[lightning_id]	= (std::string*) "Represents your ability to utilize and resist the element of lightning.",
+	ELEMENTAL_STAT_DESCRIPTIONS[holy_id]		= (std::string*) "Represents your ability to utilize and resist the holy element.",
+	ELEMENTAL_STAT_DESCRIPTIONS[dark_id]		= (std::string*) "Represents your ability to utilize and resist the dark elemen.t",
+	ELEMENTAL_STAT_DESCRIPTIONS[void_id]		= (std::string*) "Represents your ability to utilize and resist element-less magic."
+};
 
 ClassStatistics::ClassStatistics() 
 {
--- FinalProject/src/ClassStatistics.h
+++ FinalProject/src/ClassStatistics.h
@@ -15,6 +15,18 @@ enum Low_Stats_Attribute : const int
 	base_value_id, current_value_id, growth_value_id
 };
 
+enum High_Stats_ID : const int
+{
+	health_id, mana_id, skill_points_id, physical_atk_id, def_id, magic_atk_id, mind_id,
+	accuracy_id, evasion_id, critical_id
+};
+
+// Used for Elemental Attack, Resist, and Affinity
+enum Elemental_Stats_ID : const int
+{
+	fire_id, wind_id, earth_id, water_id, lightning_id, holy_id, dark_id, void_id
+};
+
 class ClassStatistics
 {
 public:
@@ -40,8 +52,15 @@ public:
 	void set_growth_value(Low_Stats_ID const &stat_id, const float &growth_value);
 
 	//Static consts
+		//Low stats
 	static const int NUM_OF_LOW_STATS = 9;
 	static const int NUM_OF_LOW_STAT_ATTRIBUTES = 3;
+		//High Stats
+	static const int NUM_OF_HIGH_LEVEL_STATS = 10;
+	static const int NUM_OF_HIGH_STAT_ATTRIBUTES = 2;
+		// Elemental Stats
+	static const int NUM_OF_ELEMENTAL_STATS = 8;
+	static const int NUM_OF_ELEMENTAL_STAT_ATTRIBUTES = 3;
 	//Balancing Static
 	static const int BASE_STAT_INCREASE_PER_LEVEL = 5;
 
@@ -50,10 +69,21 @@ public:
 
 private:
 	//static consts
+	// Low Level
 	static const int LOW_STAT_MAX_VALUE = 65535;
 	static const std::string* LOW_STAT_DESCRIPTIONS[NUM_OF_LOW_STATS];
-	
 	float m_low_level_stats[NUM_OF_LOW_STATS][NUM_OF_LOW_STAT_ATTRIBUTES];
+
+	//High Level
+	static const double HIGH_STAT_MAX_VALUE;
+	static const std::string* HIGH_STAT_DESCRIPTIONS[NUM_OF_HIGH_LEVEL_STATS];
+	float m_high_level_stats[NUM_OF_HIGH_LEVEL_STATS][NUM_OF_HIGH_STAT_ATTRIBUTES];
+
+	//Elemental
+	static const float ELEMENTAL_STAT_MAX_VALUE;
+	static const std::string* ELEMENTAL_STAT_DESCRIPTIONS[NUM_OF_ELEMENTAL_STATS];
+	float m_elemental_stats[NUM_OF_ELEMENTAL_STATS][NUM_OF_ELEMENTAL_STAT_ATTRIBUTES];
+
 
 	
 };
